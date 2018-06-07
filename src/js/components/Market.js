71c91301b5a021395b14956a41d79bbb643ff512
Ryan Naylor <ryan.naylor@hs2.org.uk>
2018-06-07 11:24:29 +0100

    First attempt at using JSS.

--- src/js/components/Market.js
+++ src/js/components/Market.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
 import MarketCard from "./MarketCard";
+import jss from 'jss'
+import preset from 'jss-preset-default'
+
+jss.setup(preset())
+
+// Create your style.
+const styles = {
+  marketCardList: {
+    listStyle: 'none',
+    display: 'flex',
+    flexWrap: 'wrap'
+  }
+}
+
+const { classes } = jss.createStyleSheet(styles).attach()
 
 class Market extends React.Component {
   render() {
     return (
       <div>
-        <MarketCard name="Test1" type="Test" cost={2}></MarketCard>
-        <MarketCard name="Test2" type="Prod"cost={4}></MarketCard>
+        <ul className={classes.marketCardList}>
+          <MarketCard name="Test1" type="Test" cost={2}></MarketCard>
+          <MarketCard name="Test2" type="Prod" cost={4}></MarketCard>
+          <MarketCard name="Test2" type="Prod" cost={4}></MarketCard>
+          <MarketCard name="Test2" type="Prod" cost={4}></MarketCard>
+          <MarketCard name="Test2" type="Prod" cost={4}></MarketCard>
+          <MarketCard name="Test2" type="Prod" cost={4}></MarketCard>
+          <MarketCard name="Test2" type="Prod" cost={4}></MarketCard>
+          <MarketCard name="Test2" type="Prod" cost={4}></MarketCard>
+          <MarketCard name="Test2" type="Prod" cost={4}></MarketCard>
+          <MarketCard name="Test2" type="Prod" cost={4}></MarketCard>
+        </ul>
       </div>
     );
   }
--- src/js/components/MarketCard.js
+++ src/js/components/MarketCard.js
@@ -1,18 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import jss from 'jss'
+import preset from 'jss-preset-default'
+
+jss.setup(preset())
+
+// Create your style.
+const styles = {
+    marketCard: {
+        width: '18%',
+        margin: "0 2% 8px 0",
+        padding: 0,
+        backgroundColor: 'red',
+        "& span": {
+            display: 'block',
+            textAlign: 'center'
+        }
+    }
+
+}
+
+const {classes} = jss.createStyleSheet(styles).attach()
 
 const MarketCard = ({ name, type, cost }) => (
-    <div>
-        <span>{name}</span>
-        <span>{type}</span>
-        <span>{cost}</span>
-    </div>
+    <li className={classes.marketCard}>
+        <div>
+            <span>{name}</span>
+        </div>
+        <div>
+            <span>{type}</span>
+        </div>
+        <div>
+            <span>{cost}</span>
+        </div>
+    </li>
 )
 
 MarketCard.propTypes = {
     name: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     cost: PropTypes.number.isRequired
-  }
+}
 
 export default MarketCard
