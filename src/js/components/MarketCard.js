7a74dd6c42d14b06468667510a1ccf38667344e2
Ryan Naylor <ryan.naylor@hs2.org.uk>
2018-06-08 13:59:00 +0100

    Added React-JSS and split the css into separate files for the components

--- src/js/components/MarketCard.js
+++ src/js/components/MarketCard.js
@@ -1,51 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import jss from 'jss'
-import preset from 'jss-preset-default'
+import injectSheet from 'react-jss'
+import styles from './MarketCard.css'
 
-jss.setup(preset())
-
-// Create your style.
-const styles = {
-    marketCard: {
-        width: '18%',
-        margin: "0 2% 8px 0",
-        padding: 0,
-        border: '1px solid black',
-        borderRadius: '5px',
-        "& span": {
-            display: 'block',
-            textAlign: 'center'
-        }
-    },
-    producer: {
-        extend: 'marketCard',
-        backgroundColor: 'lightblue'
-    },
-    grain: {
-        extend: 'producer',
-    },
-    cog: {
-        extend: 'producer',
-    },
-    cattle: {
-        extend: 'producer',
-    },
-    cafe: {
-        extend: 'marketCard',
-        backgroundColor: 'lightcoral'
-    },
-    shop: {
-        extend: 'marketCard',
-        backgroundColor: 'lightgreen'
-    }
-
-
-}
-
-const { classes } = jss.createStyleSheet(styles).attach()
-
-const MarketCard = ({ card, num, onClick }) => (
+const MarketCard = ({ classes, card, num, onClick }) => (
     <li className={classes[card.type]} onClick={onClick.bind(this, card.name)}>
         <div>
             <span>{num}</span>
@@ -66,4 +24,4 @@ MarketCard.propTypes = {
     num: PropTypes.number.isRequired
 }
 
-export default MarketCard
+export default injectSheet(styles)(MarketCard) 
